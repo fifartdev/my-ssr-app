f59e3177b942f4c2c8569121f44d4938299d8af8
fifartdev <fifartapp@gmail.com>
2025-08-24 18:39:29 +0300

    final check

--- src/app/signup/loading.jsx
+++ src/app/signup/loading.jsx
@@ -3,10 +3,10 @@ import { Skeleton } from "@/components/ui/skeleton";
 export default function loadingSignUp() {
   return (
     <div className="flex flex-col justify-center items-center h-screen px-4">
-      <Skeleton className="h-[50px] w-[300px] rounded-xl" />
-      <Skeleton className="h-[50px] w-[300px] rounded-xl" />
-      <Skeleton className="h-[50px] w-[300px] rounded-xl" />
-      <Skeleton className="h-[50px] w-[300px] rounded-xl" />
+      <Skeleton className="h-[50px] w-[300px] rounded-xl mt-2" />
+      <Skeleton className="h-[50px] w-[300px] rounded-xl mt-2" />
+      <Skeleton className="h-[50px] w-[300px] rounded-xl mt-2" />
+      <Skeleton className="h-[50px] w-[300px] rounded-xl mt-2" />
     </div>
   );
 }
