6d5c292802cd9de41d31d6a830467f59a45345f4
fifartdev <fifartapp@gmail.com>
2025-09-01 09:41:32 +0300

    ready to test on deployment again

--- src/app/lib/constants.js
+++ src/app/lib/constants.js
@@ -1,22 +1,12 @@
-// PRODUCTION
-const endpoint = process.env.APPWRITE_ENDPOINT;
-const projectId = process.env.APPWRITE_PROJECT;
-const apiKey = process.env.APPWRITE_KEY;
-const databaseId = process.env.APPWRITE_DATABASE_ID;
-const tableClientsId = process.env.APPWRITE_TABLE_CLIENTS_ID;
-const tableServiceCategoryId =
-  process.env.APPWRITE_TABLE_SERVICES_CATEGORIES_ID;
-const tableServicesId = process.env.APPWRITE_TABLE_SERVICES;
-
 // DEVELOPMENT
-// const endpoint = process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT;
-// const projectId = process.env.NEXT_PUBLIC_APPWRITE_PROJECT;
-// const apiKey = process.env.NEXT_PUBLIC_APPWRITE_KEY;
-// const databaseId = process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID;
-// const tableClientsId = process.env.NEXT_PUBLIC_APPWRITE_TABLE_CLIENTS_ID;
-// const tableServiceCategoryId =
-//   process.env.NEXT_PUBLIC_APPWRITE_TABLE_SERVICES_CATEGORIES_ID;
-// const tableServicesId = process.env.NEXT_PUBLIC_APPWRITE_TABLE_SERVICES;
+const endpoint = process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT;
+const projectId = process.env.NEXT_PUBLIC_APPWRITE_PROJECT;
+const apiKey = process.env.NEXT_PUBLIC_APPWRITE_KEY;
+const databaseId = process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID;
+const tableClientsId = process.env.NEXT_PUBLIC_APPWRITE_TABLE_CLIENTS_ID;
+const tableServiceCategoryId =
+  process.env.NEXT_PUBLIC_APPWRITE_TABLE_SERVICES_CATEGORIES_ID;
+const tableServicesId = process.env.NEXT_PUBLIC_APPWRITE_TABLE_SERVICES;
 
 export {
   endpoint,
